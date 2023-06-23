74dae8ef6e720ce03ff9743c367c754937d79130
DanieleCorrado <corrado_daniele@virgilio.it>
2023-06-23 13:59:47 +0200

    add delete and add task

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,59 @@
 <script>
-
 import axios from 'axios';
-
 export default {
   data() {
+
     return {
-      TodoList: []
+      tasks: [],
+      newTask: {
+        name: "",
+        complete: "false"
+      }
+    };
+  },
+  methods: {
+
+    onSubmit() {
+      const url = 'http://localhost/newTask.php';
+      const data = this.newTask;
+      const headers = {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      };
+
+      axios.post(url, data, headers)
+        .then(res => {
+
+          const data = res.data;
+          this.tasks = data;
+          this.newTask.name = "";
+          this.newTask.complete = "false";
+        })
+    },
+
+    deleteTask(idx) {
+
+      const url = 'http://localhost/deleteTask.php';
+      const data = { "index": idx };
+      const headers = {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      };
+      axios.post(url, data, headers)
+        .then(res => {
+          const data = res.data;
+          this.tasks = data;
+        });
     }
   },
   mounted() {
-    axios.get('http://localhost/index.php').then(r => {
-      this.TodoList = r.data;
-      console.log(this.TodoList);
-    })
-  },
+
+    axios.get("http://localhost/tasks.php")
+      .then(res => {
+
+        const data = res.data;
+        this.tasks = data;
+      });
+  }
 }
-
-
 </script>
 
 <template>
@@ -24,9 +61,20 @@ export default {
 
     <h1 id="title">Todo List</h1>
 
+    <form @submit.prevent="onSubmit" id="addtask">
+      <input type="text" name="name" v-model="newTask.name" placeholder="Aggiungi la task">
+      <input type="submit" value="ADD">
+    </form>
+
     <div id="list">
       <ul>
-        <li v-for="element in TodoList"> {{ element.name }}</li>
+        <li v-for="(task, idx) in tasks" :class="task.complete === 'true' ? 'line-through' : ''" class="flex"> {{
+          task.name }}
+          <div class="delete" @click="deleteTask(idx)">
+            <font-awesome-icon icon="fa-solid fa-trash-can" />
+          </div>
+        </li>
+
       </ul>
     </div>
   </body>
@@ -34,6 +82,31 @@ export default {
 
 <style lang="scss">
 @use '../src/css/general.scss' as *;
+
+.flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+#addtask {
+  text-align: center;
+  margin-bottom: 30px;
+
+  input {
+    padding: 10px;
+    margin-left: 10px;
+    border-radius: 5px;
+    border: none;
+  }
+}
+
+.delete {
+  color: white;
+  background-color: red;
+  padding: 10px;
+  border-radius: 5px;
+}
 
 #title {
   font-size: 90px;
@@ -49,13 +122,16 @@ export default {
   border-radius: 10px;
 
   ul {
-    // padding: 10px 0;
     list-style: none;
 
     li {
       padding: 10px;
       margin-bottom: 10px;
       border-bottom: 1px solid black;
+    }
+
+    .line-through {
+      text-decoration: line-through;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+library.add(faTrashCan)
 
-createApp(App).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
